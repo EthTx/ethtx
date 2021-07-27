ab562b20fad3fea5e56aa0d855ce712538e3bdd6
Piotr Rudnik <1010000i100@gmail.com>
2021-07-27 16:33:29 +0200

    Create new releases with use of Github CI

---
+++ utils/get_last_changelog_entry.py
@@ -0,0 +1,15 @@
+import sys, os
+
+lines = open(os.path.dirname(__file__)+"/../CHANGELOG.md").readlines()[3:]
+was_last_line_blank = False
+log = ""
+for line in lines:
+  if line == "\n":
+    if was_last_line_blank:
+     print(log)
+     sys.exit(0)
+    else: 
+      was_last_line_blank = True
+  else:
+    log += line
+    was_last_line_blank = False
