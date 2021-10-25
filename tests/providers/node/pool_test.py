4279123f0a959eed19b9e1c04883aa34e1ba3168
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-10-25 17:08:31 +0200

    fix tests

--- tests/providers/node/pool_test.py
+++ tests/providers/node/pool_test.py
@@ -10,27 +10,28 @@ GOERLI_NODE = NodeConnection("goerli", "B", False)
 
 
 class TestNodeConnectionPool:
-    @classmethod
-    def setup_class(cls):
-        cls.pool = NodeConnectionPool(nodes=MAINNET_CHAIN)
-
     def test_number_of_connections(self):
-        assert len(self.pool) == 1, "Number of connections should equal 1."
+        pool = NodeConnectionPool(nodes=MAINNET_CHAIN)
+        assert len(pool) == 1, "Number of connections should equal 1."
 
     def test_add_connection(self):
-        self.pool.add_connection(connection=GOERLI_NODE)
+        pool = NodeConnectionPool(nodes=MAINNET_CHAIN)
+        pool.add_connection(connection=GOERLI_NODE)
 
         assert (
-            GOERLI_NODE in self.pool.connections
+            GOERLI_NODE in pool.connections
         ), f"{GOERLI_NODE} should be in pool of connections."
 
     def test_get_connection(self):
-        connection = self.pool.get_connection("goerli")
+        pool = NodeConnectionPool(nodes=MAINNET_CHAIN)
+        pool.add_connection(connection=GOERLI_NODE)
+        connection = pool.get_connection("goerli")
 
         assert (
             connection[0] == GOERLI_NODE
         ), f"{GOERLI_NODE} should be in pool of connections."
 
     def test_add_wrong_type_connection(self):
+        pool = NodeConnectionPool(nodes=MAINNET_CHAIN)
         with pytest.raises(ValueError):
-            self.pool.add_connection((1, 1, 1))
+            pool.add_connection((1, 1, 1))
