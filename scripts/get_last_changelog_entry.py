4dcc7694fbbf3d9ce22bdf1e9885609caa3aa925
Tomasz Mierzwa <tomasz@makerdao.com>
2021-12-16 16:58:38 +0100

    Parameters decoding fixes (#81)
    * Parameters decoding fixes
    
    * Black formatting

--- scripts/get_last_changelog_entry.py
+++ scripts/get_last_changelog_entry.py
@@ -1,15 +1,15 @@
 import sys, os
 
-lines = open(os.path.dirname(__file__)+"/../CHANGELOG.md").readlines()[3:]
+lines = open(os.path.dirname(__file__) + "/../CHANGELOG.md").readlines()[3:]
 was_last_line_blank = False
 log = ""
 for line in lines:
-  if line == "\n":
-    if was_last_line_blank:
-     print(log)
-     sys.exit(0)
-    else: 
-      was_last_line_blank = True
-  else:
-    log += line
-    was_last_line_blank = False
+    if line == "\n":
+        if was_last_line_blank:
+            print(log)
+            sys.exit(0)
+        else:
+            was_last_line_blank = True
+    else:
+        log += line
+        was_last_line_blank = False
