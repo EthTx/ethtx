940260e7789b82dfb6f072fe33168b3d1c1b7433
Piotr Rudnik <piotr.rudnik@tfi.team>
2022-03-03 14:09:35 +0100

    Make pydantic BaseModel hashable

--- ethtx/models/base_model.py
+++ ethtx/models/base_model.py
@@ -16,3 +16,6 @@ from pydantic import BaseModel as PydanticBaseModel
 class BaseModel(PydanticBaseModel):
     class Config:
         arbitrary_types_allowed = True
+
+    def __hash__(self):
+        return hash((type(self),) + tuple(self.__dict__.values()))
