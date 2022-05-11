6e7ff143613d13ecc298bc55f3a950d0e7eb15cd
Karol Chojnowski <karolchojnowski95@gmail.com>
2022-05-11 13:58:58 +0200

    add cache

--- ethtx/providers/semantic_providers/database.py
+++ ethtx/providers/semantic_providers/database.py
@@ -39,6 +39,7 @@ class MongoSemanticsDatabase(ISemanticsDatabase):
     def get_collection_count(self) -> int:
         return len(self._db.list_collection_names())
 
+    @cache
     def get_address_semantics(
         self, chain_id, address, *, cursor_timeout_millis=None
     ) -> Dict:
@@ -50,6 +51,7 @@ class MongoSemanticsDatabase(ISemanticsDatabase):
             **self._cursor_properties(cursor_timeout_millis=cursor_timeout_millis),
         )
 
+    @cache
     def get_signature_semantics(
         self, signature_hash: str, *, cursor_timeout_millis=None
     ) -> Cursor:
@@ -74,6 +76,7 @@ class MongoSemanticsDatabase(ISemanticsDatabase):
         inserted_signature = self._signatures.insert_one(signature)
         return inserted_signature.inserted_id
 
+    @cache
     def get_contract_semantics(self, code_hash, *, cursor_timeout_millis=None) -> Dict:
         """Contract hashes are always the same, no mather what chain we use, so there is no need
         to use chain_id"""
